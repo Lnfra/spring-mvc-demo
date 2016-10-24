743231f82c0c49a6ab684a2b92dfba1618030cc6
Angeline Tan <atan009@gmail.com>
2016-10-24 16:40:49 +0800

    Binding request params from a form to a variable in controller using @RequestParam annotation

--- src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -33,6 +34,24 @@ public class HelloWorldController {
     
     //create message
     String result = "Yo! " + theName;
+    
+    //add message to the model
+    model.addAttribute("message", result);
+    
+    return "helloworld";
+  }
+  
+  //Bind studentName to theName using @RequestParam
+  @RequestMapping("/processFormVersionThree")
+  public String processFormVersionThree(
+      @RequestParam("studentName") String theName,
+      Model model) {
+    
+    //convert data to all caps
+    theName = theName.toUpperCase();
+    
+    //create message
+    String result = "Hey my friend from v3! " + theName;
     
     //add message to the model
     model.addAttribute("message", result);
